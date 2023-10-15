d662eada1be0538702585b3f554fec550a063fe7
@prakt_gorbachevrd <rodion.boy@mail.ru>
2023-10-15 22:34:04 +0300

    Init commit

---
+++ src/main/java/com/project/tmc/TmcApplication.java
@@ -0,0 +1,13 @@
+package com.project.tmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tmc/TmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
