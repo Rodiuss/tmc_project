863eced10cf87de1307f7f57c530bace72a168eb
@prakt_gorbachevrd <rodion.boy@mail.ru>
2023-10-29 16:22:04 +0300

    Add fields on form

--- src/main/java/com/project/tmc/model/User.java
+++ src/main/java/com/project/tmc/model/User.java
@@ -23,28 +23,28 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public class User implements Comparable<User> {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name="id")
+    @Column(name="id", nullable = false)
     private Long id;
 
-    @Column(name="username")
+    @Column(name="username", nullable = false)
     private String username;
 
-    @Column(name="email")
+    @Column(name="email", nullable = false)
     private String email;
 
-    @Column(name="password")
+    @Column(name="password", nullable = false)
     private String password;
 
-    @Column(name="surname")
+    @Column(name="surname", nullable = false)
     private String surname;
 
-    @Column(name="name")
+    @Column(name="name", nullable = false)
     private String name;
 
-    @Column(name="patronymic")
+    @Column(name="patronymic", nullable = false)
     private String patronymic;
 
-    @Column(name="ind_tax_num")
+    @Column(name="ind_tax_num", nullable = false)
     private String IndividualTaxpayerNumber;
 
     @ManyToMany
